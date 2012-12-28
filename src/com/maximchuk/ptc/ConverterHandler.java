0647d1c35cc58037e1c17cd69b4bcf2e98495014
Maxim L. Maximchuk <mlmaximchuk@gmail.com>
2012-12-28 22:59:58 +0200

    Modified input app params. Changed to properties file

--- src/com/maximchuk/ptc/ConverterHandler.java
+++ src/com/maximchuk/ptc/ConverterHandler.java
@@ -6,6 +6,7 @@ import com.maximchuk.ptc.parser.impl.ThemerollerZipParser192;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.Properties;
 
 /**
  * Main converter handler
@@ -13,6 +14,11 @@ import java.io.IOException;
  * @author Maxim L. Maximchuk
  */
 public class ConverterHandler {
+    public static final String SRC_FILENAME_KEY = "source.filename";
+    public static final String THEME_NAME_KEY = "theme.name";
+    public static final String OUTPUT_DIR_KEY = "output.dir";
+
+    private static final String DEFAULT_OUTPUT_DIR = "out_theme";
 
     /**
      * Hided class constructor
@@ -25,25 +31,30 @@ public class ConverterHandler {
      * Main processing method
      * generates primefaces theme jar from themeroller jar
      *
-     * @param sourceFilename source themeroller zip file name
-     * @param themeName your theme preferred name, if null then making from source default name
-     * @param outDir result output directory
+     * @param themeProps theme properties
      */
-    public static void process(String sourceFilename, String themeName, String outDir) {
-        try {
-            ThemerollerZipParser parser = new ThemerollerZipParser192(new File(sourceFilename));
-            if (parser.validate()) {
-                parser.setThemeName(themeName);
-                if (parser.parse()) {
-                    buildTheme(parser, outDir);
+    public static void process(Properties themeProps) {
+        String filename = themeProps.getProperty(SRC_FILENAME_KEY);
+        String themeName = themeProps.getProperty(THEME_NAME_KEY);
+        String outDir = themeProps.getProperty(OUTPUT_DIR_KEY);
+        if (filename != null) {
+            try {
+                ThemerollerZipParser parser = new ThemerollerZipParser192(new File(filename));
+                if (parser.validate()) {
+                    parser.setThemeName(themeName);
+                    if (parser.parse()) {
+                        buildTheme(parser, outDir != null? outDir: DEFAULT_OUTPUT_DIR);
+                    } else {
+                        System.err.println("Invalid themeroller zip");
+                    }
                 } else {
-                    System.err.println("Invalid themeroller zip");
+                    System.err.println("Unsupported themeroller version");
                 }
-            } else {
-                System.err.println("Unsupported themeroller version");
+            } catch (Exception ex) {
+                ex.printStackTrace();
             }
-        } catch (Exception ex) {
-            ex.printStackTrace();
+        } else {
+            System.err.println("Unspecified source file name");
         }
     }
 
--- src/com/maximchuk/ptc/Launcher.java
+++ src/com/maximchuk/ptc/Launcher.java
@@ -1,6 +1,10 @@
 package com.maximchuk.ptc;
 
 
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
+
 /**
  * Application launcher with main method
  *
@@ -10,12 +14,20 @@ public class Launcher {
     private static final String OUTPUT_DIR = "out_theme";
 
     public static void main(String[] args) {
-        if (args.length > 0) {
-            String themeName = null;
-            if (args.length > 1) {
-                themeName = args[1];
+        if (args.length == 1) {
+            if (args[0].equals("-w")) {
+
+            } else {
+                try {
+                    Properties props = new Properties();
+                    props.load(new FileInputStream(args[0]));
+                    ConverterHandler.process(props);
+                } catch (IOException e) {
+                    System.err.printf("Properties with name '%s' is not found\n", args[0]);
+                } catch (Exception e) {
+                    System.err.println("Incorrect properties format");
+                }
             }
-            ConverterHandler.process(args[0], themeName, OUTPUT_DIR);
         }
     }
 
